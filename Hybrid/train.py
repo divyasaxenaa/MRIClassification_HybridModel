75a5991825d3c30bc34bef08589438cfd89c313f
Divya Saxena <54782918+divyasaxenaa@users.noreply.github.com>
2021-01-26 13:03:50 -0600

    Add files via upload

--- Hybrid/train.py
+++ Hybrid/train.py
@@ -13,19 +13,19 @@ from dataset_split import *
 from graphplt import *
 
 l2_regularization = 4e-4
-total_classes = 2#Health and Patient
+total_classes = 2  # Health and Patient
+
 
 class GraphCNNNet(Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.conv1 = GraphConv(128, activation='elu', kernel_regularizer=l2(l2_regularization))
         self.conv2 = GraphConv(64, activation='elu', kernel_regularizer=l2(l2_regularization))
-        self.conv_1 =  Conv1D(32, kernel_size=(2), kernel_initializer = 'he_uniform')
+        self.conv_1 = Conv1D(32, kernel_size=(2), kernel_initializer='he_uniform')
         self.dropout = Dropout(0.5)
         self.flatten = Flatten()
         self.fc1 = Dense(512, activation='relu')
         self.fc2 = Dense(total_classes, activation='softmax')
-
 
     def call(self, inputs):
         x, fltr = inputs
@@ -38,6 +38,7 @@ class GraphCNNNet(Model):
         output = self.fc2(output)
         return output
 
+
 class GraphNet(Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -46,7 +47,6 @@ class GraphNet(Model):
         self.flatten = Flatten()
         self.fc1 = Dense(512, activation='relu')
         self.fc2 = Dense(total_classes, activation='softmax')
-
 
     def call(self, inputs):
         x, fltr = inputs
@@ -61,12 +61,11 @@ class GraphNet(Model):
 class CNNNet(Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.conv1 = Conv1D(64,kernel_size=(2), activation='elu', kernel_regularizer=l2(l2_regularization))
-        self.conv2 = Conv1D(32, kernel_size=(2),activation='elu', kernel_regularizer=l2(l2_regularization))
+        self.conv1 = Conv1D(64, kernel_size=(2), activation='elu', kernel_regularizer=l2(l2_regularization))
+        self.conv2 = Conv1D(32, kernel_size=(2), activation='elu', kernel_regularizer=l2(l2_regularization))
         self.flatten = Flatten()
         self.fc1 = Dense(512, activation='relu')
         self.fc2 = Dense(total_classes, activation='softmax')
-
 
     def call(self, inputs):
         x, fltr = inputs
@@ -77,16 +76,16 @@ class CNNNet(Model):
         output = self.fc2(output)
         return output
 
+
 class GraphRNNNet(Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.conv1 = GraphConv(128, activation='elu', kernel_regularizer=l2(l2_regularization))
         self.conv2 = GraphConv(64, activation='elu', kernel_regularizer=l2(l2_regularization))
-        self.conv_1 =  LSTM(32, kernel_initializer = 'he_uniform')
+        self.conv_1 = LSTM(32, kernel_initializer='he_uniform')
         self.flatten = Flatten()
         self.fc1 = Dense(512, activation='relu')
         self.fc2 = Dense(total_classes, activation='softmax')
-
 
     def call(self, inputs):
         x, fltr = inputs
@@ -97,31 +96,7 @@ class GraphRNNNet(Model):
         output = self.fc1(output)
         output = self.fc2(output)
         return output
-#divya
-#
-# class RNNNet(Model):
-#     def __init__(self, **kwargs):
-#         print(**kwargs)
-#         super().__init__(**kwargs)
-#
-#         self.conv1 = LSTM(128, kernel_initializer = 'he_uniform')
-#         self.conv2 = LSTM(64, kernel_initializer = 'he_uniform')
-#         self.conv_1 =  LSTM(32, kernel_initializer = 'he_uniform')
-#         self.flatten = Flatten()
-#         self.fc1 = Dense(512, activation='relu')
-#         self.fc2 = Dense(total_classes, activation='softmax')
-#
-#
-#     def call(self, inputs):
-#         print("1")
-#         x, fltr = inputs
-#         x = self.conv1(x)
-#         x = self.conv2(x)
-#         x = self.conv_1(x)
-#         output = self.flatten(x)
-#         output = self.fc1(output)
-#         output = self.fc2(output)
-#         return output
+
 
 
 def train_model(model_name):
@@ -132,8 +107,8 @@ def train_model(model_name):
     test_loss = []
     test_acc = []
     learning_rate = 1e-3  # Learning rate for Adam
-    batch_size = 32       # Batch size
-    epochs = 1      # Number of training epochs dsx 1000
+    batch_size = 50  # Batch size
+    epochs = 1  # Number of training epochs dsx 1000
     train_data, train_label, val_data, val_label, test_data, test_label, graph_created = load_data()
     train_data, val_data, test_data = train_data[..., None], val_data[..., None], test_data[..., None]
 
@@ -147,18 +122,17 @@ def train_model(model_name):
         model = CNNNet()
     elif model_name == "GraphRNNNet":
         model = GraphRNNNet()
-#divya
-    # elif model_name == "RNNNet":
-    #     model = GraphRNNNet("RNNNet")
-    
+
+
     STEPS_PER_EPOCH = 1
-    lr_schedule = InverseTimeDecay(0.001, 
-        decay_steps=STEPS_PER_EPOCH*10, 
-        decay_rate=1, 
-        staircase=False)
+    lr_schedule = InverseTimeDecay(0.001,
+                                   decay_steps=STEPS_PER_EPOCH * 10,
+                                   decay_rate=1,
+                                   staircase=False)
     optimizer = Adam(lr_schedule)
     loss_fn = SparseCategoricalCrossentropy()
     accuracy_fn = SparseCategoricalAccuracy()
+
     # Training step
     @tf.function
     def train(x, y):
@@ -174,7 +148,6 @@ def train_model(model_name):
         model.summary()
         return loss, acc
 
-
     # Evaluation step
     @tf.function
     def evaluate(x, y):
@@ -183,7 +156,6 @@ def train_model(model_name):
         loss += sum(model.losses)
         acc = accuracy_fn(y, predictions)
         return loss, acc
-
 
     curent_batch = 0
     batches_in_epoch = int(np.ceil(train_data.shape[0] / batch_size))
@@ -226,10 +198,15 @@ if __name__ == "__main__":
     train_loss2, train_acc2, val_loss2, val_acc2, test_loss2, test_acc2, model_name2 = train_model("GraphCNNNet")
     train_loss3, train_acc3, val_loss3, val_acc3, test_loss3, test_acc3, model_name3 = train_model("CNNNet")
     train_loss4, train_acc4, val_loss4, val_acc4, test_loss4, test_acc4, model_name4 = train_model("GraphRNNNet")
-    #divya train_loss5, train_acc5, val_loss5, val_acc5, test_loss5, test_acc5, model_name5 = train_model("RNNNet")
-    graphplt(train_loss1,train_loss2,train_loss3,train_loss4, "Train_loss ", "No. of epochs", "Loss During Training")
-    graphplt(train_acc1,train_acc2,train_acc3,train_acc4,"Train_Accuracy ", "No. of epochs", "Accuracy During Training")
-    graphplt(val_loss1,val_loss2,val_loss3,val_loss4, "Validation_loss ", "No. of epochs", "Loss During Validation")
-    graphplt(val_acc1,val_acc2,val_acc3,val_acc4 ,"Validation_Accuracy ", "No. of epochs", "Accuracy During Validation")
-    graphplt(test_loss1,test_loss2,test_loss3,test_loss4,"Testing_loss  ", "No. of epochs", "Loss During Testing")
-    graphplt(test_acc1,test_acc2,test_acc3,test_acc4, "Testing_Accuracy  ", "No. of epochs", "Accuracy During Testing")
+    graphplt(train_loss1, train_loss2, train_loss3, train_loss4, "Train_loss ", "No. of epochs",
+             "Loss During Training")
+    graphplt(train_acc1, train_acc2, train_acc3, train_acc4, "Train_Accuracy ", "No. of epochs",
+             "Accuracy During Training")
+    graphplt(val_loss1, val_loss2, val_loss3, val_loss4, "Validation_loss ", "No. of epochs",
+             "Loss During Validation")
+    graphplt(val_acc1, val_acc2, val_acc3, val_acc4, "Validation_Accuracy ", "No. of epochs",
+             "Accuracy During Validation")
+    graphplt(test_loss1, test_loss2, test_loss3, test_loss4, "Testing_loss  ", "No. of epochs",
+             "Loss During Testing")
+    graphplt(test_acc1, test_acc2, test_acc3, test_acc4, "Testing_Accuracy  ", "No. of epochs",
+             "Accuracy During Testing")
