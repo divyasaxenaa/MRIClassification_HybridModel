8ea7aa6c1282ebf90c6215932cf8ea243b71a903
Divya Saxena <54782918+divyasaxenaa@users.noreply.github.com>
2021-01-26 13:06:33 -0600

    Add files via upload

--- Hybrid/show_image.py
+++ Hybrid/show_image.py
@@ -7,7 +7,7 @@ import os
 path = os.getcwd()
 
 
-def resize_28_28(image, ideal_shape = (28, 28)):
+def resize_112_112(image, ideal_shape = (112, 112)):
     img_new2 = resize(image, ideal_shape, anti_aliasing=True)
     return img_new2
 
@@ -21,7 +21,7 @@ def shw_img_normal(data,label):
 
 def shw_img_resize(data,label):
     img = np.asarray(np.loadtxt(data))
-    img  = resize_28_28(img)
+    img  = resize_112_112(img)
     plt.figure(num=label)
     plt.imshow(img, cmap='gray')
     plt.show()
@@ -30,10 +30,10 @@ def shw_img_resize(data,label):
 if __name__ == "__main__":
     plt.rcParams['toolbar'] = 'None'
     home = str(Path.home())
-    data_health =  path+"/CSE6389_project2/CSE6389_project2/Training/Health_raw/common_fiber_matrix8.txt"
-    data_patient =  path+"/CSE6389_project2/CSE6389_project2/Training/Patient_raw/common_fiber_matrix8.txt"
+    data_health =  path+"/CSE6389_project2/CSE6389_project2/Training/Health_raw/average_fmri_feature_matrix1.txt"
+    data_patient =  path+"/CSE6389_project2/CSE6389_project2/Training/Patient_raw/average_fmri_feature_matrix1.txt"
     shw_img_normal(data_patient,"Patient Sample Original(150*150)")
     shw_img_normal(data_health,"Healthy Sample Original(150*150)")
-    shw_img_resize(data_patient, "Patient Sample Resized(28*28)")
-    shw_img_resize(data_health, "Healthy Sample Resized(28*28)")
+    shw_img_resize(data_patient, "Patient Sample Resized(112*112)")
+    shw_img_resize(data_health, "Healthy Sample Resized(112*112)")
 
